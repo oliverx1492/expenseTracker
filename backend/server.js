dcea8904cb38908206401844b4884afbd9fe0c7b
Oliver <oliverx1492@protonmail.com>
2024-10-29 14:16:30 +0100

    Änderungen vorgneommen

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,20 @@ app.post("/addExpense", async (req,res) => {
         return res.status(200)
 })
 
+app.post("/delete", async (req,res) => {
+    const {id} = req.body
+
+    try {
+        const query = await sql`DELETE FROM expenses WHERE ID = ${id} RETURNING*`
+        res.status(200).json({message: "Eintrag gelöscht"})
+    }
+
+    catch(error) {
+        console.log(error)
+        res.status(500).json({message: "Fehler beim Löschen aufgetreten"})
+    }
+})
+
 app.get("/", async (req,res) => {
 
     try {
@@ -42,6 +56,18 @@ app.get("/", async (req,res) => {
     catch(error) {
         console.log("FEHLER AUFGERTETEN: ", error)
         res.status(500).json({message: "Fehler aufgretreten: ", error})
+    }
+})
+
+app.get("/checkStatus", async (req,res) => {
+    try {
+        const query = await sql`SELECT 1`
+        res.status(200).json({message: "Server ist Live"})
+    }
+
+    catch(error) {
+        console.log(error)
+        res.status(500).json({message: "Datenbank nicht erreichbar"})
     }
 })
 
